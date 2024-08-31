68319ffb6ab5d5cbfc08c748120a6e7ca7d94bd9
RS <RS@LAPTOP-VCPGANQJ>
2024-08-30 20:23:03 -0600

    Practica 02 - Alarma agregada

--- app/src/main/java/com/example/mov2_practica01/MainActivity.kt
+++ app/src/main/java/com/example/mov2_practica01/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mov2_practica01
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -8,8 +9,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.MenuItem
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -18,26 +21,42 @@ class MainActivity : AppCompatActivity() {
     private lateinit var comprar: ImageButton
     private lateinit var consultar: ImageButton
     private lateinit var capturar: ImageButton
+    private lateinit var alarma: ImageButton
 
     private val CHANNEL_ID = "Canal_notificacion"
     private val titleAPP = "SHELL SHOCK"
     private val notificationId = 1
     private val title = "Descuento"
 
+    private lateinit var intent: Intent
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var alarMqr: AlarmManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Barra para regresar
         setSupportActionBar(findViewById(R.id.barraMenu))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         comprar = findViewById(R.id.btnComprar)
         consultar = findViewById(R.id.btnConsultar)
         capturar = findViewById(R.id.btnCapturar)
+        alarma = findViewById(R.id.btnAlarma)
 
         createNotificationChannel()
 
+        //Abrir alarma activity
+        intent = Intent(this,AlarmaActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        pendingIntent = PendingIntent.getActivity(this,0, intent,PendingIntent.FLAG_IMMUTABLE)
+
+        alarMqr = (applicationContext.getSystemService(Context.ALARM_SERVICE) as? AlarmManager)!!
+
+        //FUNCIONAMIENTO DE BOTONES
         comprar.setOnClickListener {
             val intent = Intent(this,ComprarActivity::class.java)
             startActivity(intent)
@@ -54,6 +73,10 @@ class MainActivity : AppCompatActivity() {
 
         capturar.setOnClickListener {
             descargarDescuento()
+        }
+
+        alarma.setOnClickListener {
+            lanzarAlarma15seg()
         }
     }
     private fun createNotificationChannel(){
@@ -98,7 +121,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }, 5000)
     }
-
+    private fun lanzarAlarma15seg(){
+        alarMqr.set(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            SystemClock.elapsedRealtime() + 15 * 1000,
+            pendingIntent
+        )
+        Toast.makeText(applicationContext,"Recordatorio lanzado, 15 segundos restantes.", Toast.LENGTH_SHORT).show()
+    }
     //Regresar al login
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
